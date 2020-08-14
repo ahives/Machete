94246d00d1bfbd41778171df47500977915c9096
Dominique Plante <dominique.plante@gmail.com>
2020-08-14 16:57:42 -0700

    Update field type from string to int

--- src/Machete.X12Schema/V5010/Segments/H3.cs
+++ src/Machete.X12Schema/V5010/Segments/H3.cs
@@ -14,6 +14,6 @@ namespace Machete.X12Schema.V5010
         
         Value<string> VentInstructionCode { get; }
         
-        Value<string> TariffApplicationCode { get; }
+        Value<int> TariffApplicationCode { get; }
     }
 }
